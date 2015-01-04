eb5ff99480bae06e33526fb68738b12793b2279b
Sergino Delia <serge716@gmail.com>
2015-01-04 17:37:01 -0500

    finished tic tac toe game

--- t_t_t.rb
+++ t_t_t.rb
@@ -1,251 +1,69 @@
-seperator_one = "---+"
-seperator_two = "---+"
-seperator_three = "---"
+# draw board
+# player picks a number from an empty square
+#   assign an X to empty square
+# computer picks a number from empty square
+#   assin O to empty square
+# check for winner after every pic
+require "pry"
 
-seperator = seperator_one + seperator_two + seperator_three
+def initialize_board
+  b = {}
+  (1..9).each {|k, v| b[k] = " "}
+  b
+end
+board = initialize_board
 
-value_for_grid1 = "   "
-value_for_grid2 = "|   "
-value_for_grid3 = "|   "
-
-# print_top_row_selection = value_for_grid1 + value_for_grid2 + value_for_grid3
-
-# seperator = seperator1_grid1_1 + seperator1_grid1_2 + seperator1_grid1_3
-
-value_for_grid4 = "   "
-value_for_grid5 = "|   "
-value_for_grid6 = "|   "
-
-# print_middle_row_selection = value_for_grid4 + value_for_grid5 + value_for_grid6
-
-# seperator = seperator1_grid1_1 + seperator1_grid1_2 + seperator1_grid1_3
-
-value_for_grid7 = "   "
-value_for_grid8 = "|   "
-value_for_grid9 = "|   "
-
-# print_bottom_row_selection = value_for_grid7 + value_for_grid8 + value_for_grid9
-
-puts value_for_grid1 + value_for_grid2 + value_for_grid3
-puts seperator
-puts value_for_grid4 + value_for_grid5 + value_for_grid6
-puts seperator
-puts value_for_grid7 + value_for_grid8 + value_for_grid9
-
-  # case my_choice
-  # when "1"
-  #   value_for_grid1 = " X "
-  # when "2"
-  #   value_for_grid2 = "| X "
-  # when "3"
-  #   value_for_grid3 = "| X "
-  # when "4"
-  #   value_for_grid4 = " X "
-  # when "5"
-  #   value_for_grid5 = "| X "
-  # when "6"
-  #   value_for_grid6 = "| X "
-  # when "7"
-  #   value_for_grid7 = " X "
-  # when "8"
-  #   value_for_grid8 = "| X "
-  # when "9"
-  #   value_for_grid9 = "| X "
-  # end
-
-def computer_selection_logic(user_choice )
-  
+def draw_board(b)
+  system "clear"
+  puts " #{b[1]} | #{b[2]} | #{b[3]} "
+  puts "---+---+---"
+  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts "---+---+---"
+  puts " #{b[7]} | #{b[8]} | #{b[9]} "
 end
 
-possible_choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-loop do
-  my_choice = gets.chomp.to_i
-  user_choice = my_choice - 1
-
-  possible_choices.slice!(user_choice)
-  puts "**********************"
-  puts possible_choices
-
-  puts "***************************"
-
-  computer_choice = possible_choices.sample
-  # computer_selection_logic = computer_choice 
-
-  puts "**********************"
-
-  # puts puts computer_selection_logic
-  puts "*********************"
-  possible_choices.slice!(computer_choice)
-  puts
-  puts
-  puts
-  puts possible_choices
-  
-  if my_choice == 1
-    value_for_grid1 = " X "
-
-    # if value_for_grid1 == " X "
-    #   computer_choice = rand(1..3)
-    #   if computer_choice == 1 && (value_for_grid2 == "|   ")
-    #     value_for_grid2 = "| O "
-    #   elsif computer_choice == 2 && value_for_grid4 == "   "
-    #     value_for_grid4 = " O "
-    #   elsif computer_choice == 3 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 2
-    value_for_grid2 = "| X "
-    
-    # if value_for_grid2 == "| X "
-    #   computer_choice = rand(1..4)
-    #   if computer_choice == 1 && value_for_grid1 == "   "
-    #     value_for_grid1 = " O "
-    #   elsif computer_choice == 2 && value_for_grid3 == "|   "
-    #     value_for_grid3 = "| O "
-    #   elsif computer_choice == 3 && value_for_grid4 == "   "
-    #     value_for_grid4 = " O "
-    #   elsif computer_choice == 4 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 3
-    value_for_grid3 = "| X "
-
-    # if value_for_grid3 == "| X "
-    #   computer_choice = rand(1..3)
-    #   if computer_choice == 1 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   elsif computer_choice == 2 && value_for_grid6 == "|   "
-    #     value value_for_grid6 = "| O "
-    #   elsif computer_choice == 3 and value_for_grid2 == "|   "
-    #     value_for_grid2 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 4
-    value_for_grid4 = " X "
-
-    # if value_for_grid4 == " X "
-    #   computer_choice = rand(1..4)
-    #   if computer_choice == 1 && value_for_grid1 == "   "
-    #     value_for_grid1 = " O "
-    #   elsif computer_choice == 2 && value_for_grid3 == "|   "
-    #     value_for_grid3 = "| O "
-    #   elsif computer_choice == 3 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   elsif computer_choice == 4 && value_for_grid9 == "|   "
-    #     value_for_grid9 = "| O "
-    #   end
-    # end 
-
-  elsif my_choice == 5
-    value_for_grid5 = "| X "
-
-    # if value_for_grid5 == "| X "
-    #   computer_choice = rand(1..4)
-    #   if computer_choice == 1 && value_for_grid1 == "   "
-    #     value_for_grid1 = " O "
-    #   elsif computer_choice == 2 && value_for_grid3 == "|   "
-    #     value_for_grid3 = "| O "
-    #   elsif computer_choice == 3 && value_for_grid7 == "   "
-    #     value_for_grid7 = " O "
-    #   elsif computer_choice == 4 && value_for_grid9 == "|   "
-    #     value_for_grid9 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 6
-    value_for_grid6 = "| X "
-
-    # if value_for_grid6 == "| X "
-    #   computer_choice = rand(1..7)
-    #   if computer_choice == 1 && value_for_grid1 == "   "
-    #     value_for_grid1 = " O "
-    #   elsif computer_choice == 2 && value_for_grid2 == "|   "
-    #     value_for_grid2 = "| O "
-    #   elsif computer_choice == 3 && value_for_grid3 == "|   "
-    #     value_for_grid3 = "| O "
-    #   elsif computer_choice == 4 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   elsif computer_choice == 5 && value_for_grid7 == "   "
-    #     value_for_grid7 = " O "
-    #   elsif computer_choice == 6 && value_for_grid8 == "|   "
-    #     value_for_grid8 = "| O "
-    #   elsif computer_choice == 7 && value_for_grid9 == "|   "
-    #     value_for_grid9 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 7
-    value_for_grid7 = " X "
-
-    # if value_for_grid7 == " X "
-    #   computer_choice = rand(1..4)
-    #   if computer_choice == 1 && value_for_grid1 == "   "
-    #     value_for_grid1 = " O "
-    #   elsif computer_choice == 2 && value_for_grid3 == "|   "
-    #     value_for_grid3 = "| O "
-    #   elsif computer_choice == 3 && value_for_grid5 == "|   "
-    #     value_for_grid5 = "| O "
-    #   elsif computer_choice == 4 && value_for_grid9 == "|   "
-    #     value_for_grid9 = "| O "
-    #   end
-    # end
-
-  elsif my_choice == 8
-    value_for_grid8 = "| X "
-
-    # if value_for_grid8 == "| X " && value_for_grid5 == "| X "
-    #   computer_choice = 2
-    #   if computer_choice == 2
-    #     value_for_grid2 = "| O "
-    #   end
-    # else
-    #   computer_choice = 1
-    #   if computer_choice == 1
-    #     value_for_grid1 = " O "
-    #   end
-    # end
-
-  elsif my_choice == 9
-    value_for_grid9 = "| X "
-  end
-
-  if computer_choice == 1
-    value_for_grid1 = " O "
-  elsif computer_choice == 2
-    value_for_grid2 = "| O "
-  elsif computer_choice == 3
-    value_for_grid3 = "| O "
-  elsif computer_choice == 4
-    value_for_grid4 = " O "
-  elsif computer_choice == 5
-    value_for_grid5 = "| O "
-  elsif computer_choice == 6
-    value_for_grid6 = "| O "
-  elsif computer_choice == 7
-    value_for_grid7 = " O "
-  elsif computer_choice == 8
-    value_for_grid8 = "| O "
-  elsif computer_choice == 9
-    value_for_grid9 = "| O "
-  end
-
-  puts value_for_grid1 + value_for_grid2 + value_for_grid3
-  puts seperator
-  puts value_for_grid4 + value_for_grid5 + value_for_grid6
-  puts seperator
-  puts value_for_grid7 + value_for_grid8 + value_for_grid9
+def empty_position(b)
+  b.select {|k, v| v == " "}.keys
 end
 
-puts possible_choices
+def player_picks_number(b)
+  begin
+  puts "Pick a position (1-9):"
+  position = gets.chomp.to_i
+end until empty_position(b).include?(position)
+  b[position] = "X"
+end
 
-# puts print_top_row_selection
-# puts seperator
-# puts middle_row_two
-# puts seperator
-## puts bottom_row_two
+
+def computer_picks_number(b)
+  position = empty_position(b).sample
+  b[position] = "O"
+end
+
+def check_winner(b)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+      return 'Player'
+    elsif b[line[0]] == 'O' and b[line[1]] == 'O' and b[line[2]] == 'O'
+      return 'Computer'
+    else
+      return nil
+    end
+  end
+end
+
+begin
+  draw_board(board)
+
+  player_picks_number(board)
+  draw_board(board)
+  computer_picks_number(board)
+  winner = check_winner(board)
+end until winner || empty_position(board).empty?
+
+if winner
+  puts "#{winner} Won!"
+else
+  puts "Its a tie"
+end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,223 +1,65 @@
-# # Display Tic Tac Toe Board
-# # Place pick a number between 1 and 9 and it shows an X on the location
-# # Computer pick a random 0 to counter player
-# # 5 Turns for the player
-# # 4 Turns for Computer
-# # Winner = 3 in a row 
- 
-# # choices = {"1" => " ", "2" => " ", "3" => " ", "4" => " ", "5" => " ",
-# #             "6" => " ", "7" => " ", "8" => " ", "9" => " "}
+# draw board
+# player picks a number from an empty square
+#   assign an X to empty square
+# computer picks a number from empty square
+#   assin O to empty square
+# check for winner after every pic
+require "pry"
 
-# position1_grid1_1 = "     "
-# position1_grid2_1 = "|     "
-# position1_grid3_1 = "|     "
+def initialize_board
+  b = {}
+  (1..9).each {|k, v| b[k] = " "}
+  b
+end
+board = initialize_board
 
-# top_row_one = position1_grid1_1 + position1_grid2_1 + position1_grid3_1
-
-# value_for_grid1 = "     "
-# value_for_grid2 = "|     "
-# value_for_grid3 = "|     "
-
-# print_top_row_selection = value_for_grid1 + value_for_grid2 + value_for_grid3
-
-# position1_grid1_3 = "     "
-# position1_grid2_3 = "|     "
-# position1_grid3_3 = "|     "
-
-# top_row_three = position1_grid1_3 + position1_grid2_3 + position1_grid3_3
-
-# seperator1_grid1_1 = "-----+"
-# seperator1_grid1_2 = "-----+"
-# seperator1_grid1_3 = "-----"
-
-# seperator_one = seperator1_grid1_1 + seperator1_grid1_2 + seperator1_grid1_3
-
-# ##########POSITION 2#############
-# position2_grid1_1 = "     "
-# position2_grid2_1 = "|     "
-# position2_grid3_1 = "|     "
-
-# middle_row_one = position2_grid1_1 + position2_grid2_1 + position2_grid3_1
-
-# position2_grid1_2 = "     "
-# position2_grid2_2 = "|     "
-# position2_grid3_2 = "|     "
-
-# middle_row_two = position2_grid1_2 + position2_grid2_2 + position2_grid3_2
-
-# position2_grid1_3 = "     "
-# position2_grid2_3 = "|     "
-# position2_grid3_3 = "|     "
-
-# middle_row_three = position2_grid1_3 + position2_grid2_3 + position2_grid3_3
-
-# seperator2_grid1_1 = "-----+"
-# seperator2_grid1_2 = "-----+"
-# seperator2_grid1_3 = "-----"
-
-# seperator_two = seperator2_grid1_1 + seperator2_grid1_2 + seperator2_grid1_3
-
-# #############POSITION 3#########
-
-# position3_grid1_1 = "     "
-# position3_grid2_1 = "|     "
-# position3_grid3_1 = "|     "
-
-# bottom_row_one = position3_grid1_1 + position3_grid2_1 + position3_grid3_1
-
-# position3_grid1_2 = "     "
-# position3_grid2_2 = "|     "
-# position3_grid3_2 = "|     "
-
-# bottom_row_two = position3_grid1_2 + position3_grid2_2 + position3_grid3_2
-
-# position3_grid1_3 = "     "
-# position3_grid2_3 = "|     "
-# position3_grid3_3 = "|     "
-
-# bottom_row_three = position3_grid1_3 + position3_grid2_3 + position3_grid3_3
-
-# # choices = {"1" => " ", "2" => " ", "3" => " ", "4" => " ", "5" => " ",
-# #             "6" => " ", "7" => " ", "8" => " ", "9" => " "}
-
-# # game_board = [top_row_one, top_row_two, top_row_three, seperator_one, middle_row_one, 
-# #               middle_row_two, middle_row_three, seperator_two, bottom_row_one, bottom_row_two, 
-# #               bottom_row_three]
-
-#   my_choice = gets.chomp
-
-#   case my_choice
-#   when "1"
-#     value_for_grid1 = "  X  "
-#     # choices["1"] = "X"
-#   when "2"
-#     value_for_grid2 = "  X  "
-#     # choices["2"] = "X"
-#   when "3"
-#     value_for_grid3 = "  X  "
-#     # choices["3"] = "X"
-#   when "4"
-#     # choices["4"] = "X"
-#   when "5"
-#     # choices["5"] = "X"
-#   when "6"
-#     # choices["6"] = "X"
-#   when "7"
-#     # choices["7"] = "X"
-#   when "8"
-#     # choices["8"] = "X"
-#   when "9"
-#     # choices["9"] = "X"
-#   end
-
-# #puts choices
-# puts value_for_grid1
-# puts value_for_grid2
-# puts value_for_grid3
-
-# game_board = [top_row_one, print_top_row_selection, top_row_three, seperator_one, middle_row_one, 
-#               middle_row_two, middle_row_three, seperator_two, bottom_row_one, bottom_row_two, 
-#               bottom_row_three]
-
-# puts game_board
-
-# # position1_grid1_1 = "     "
-# # position1_grid2_1 = "|     "
-# # position1_grid3_1 = "|     "
-
-# # puts position1_grid1_1 + position1_grid2_1 + position1_grid3_1
-
-# # position1_grid1_2 = "  #{choices["1"]}  "
-# # position1_grid2_2 = "|  #{choices["2"]}  "
-# # position1_grid3_2 = "|  #{choices["3"]}  "
-
-# # puts position1_grid1_2 + position1_grid2_2 + position1_grid3_2
-
-# # position1_grid1_3 = "     "
-# # position1_grid2_3 = "|     "
-# # position1_grid3_3 = "|     "
-
-# # puts position1_grid1_3 + position1_grid2_3 + position1_grid3_3
-
-# # seperator1_grid1_1 = "-----+"
-# # seperator1_grid1_2 = "-----+"
-# # seperator1_grid1_3 = "-----"
-
-# # puts seperator1_grid1_1 + seperator1_grid1_2 + seperator1_grid1_3
-
-# # ##########POSITION 2#############
-# # position2_grid1_1 = "     "
-# # position2_grid2_1 = "|     "
-# # position2_grid3_1 = "|     "
-
-# # puts position2_grid1_1 + position2_grid2_1 + position2_grid3_1
-
-# # position2_grid1_2 = "  #{choices["4"]}  "
-# # position2_grid2_2 = "|  #{choices["5"]}  "
-# # position2_grid3_2 = "|  #{choices["6"]}  "
-
-# # puts position2_grid1_2 + position2_grid2_2 + position2_grid3_2
-
-# # position2_grid1_3 = "     "
-# # position2_grid2_3 = "|     "
-# # position2_grid3_3 = "|     "
-
-# # puts position2_grid1_3 + position2_grid2_3 + position2_grid3_3
-
-# # seperator2_grid1_1 = "-----+"
-# # seperator2_grid1_2 = "-----+"
-# # seperator2_grid1_3 = "-----"
-
-# # puts seperator2_grid1_1 + seperator2_grid1_2 + seperator2_grid1_3
-
-# # #############POSITION 3#########
-
-# # position3_grid1_1 = "     "
-# # position3_grid2_1 = "|     "
-# # position3_grid3_1 = "|     "
-
-# # puts position3_grid1_1 + position3_grid2_1 + position3_grid3_1
-
-# # position3_grid1_2 = "  #{choices["7"]}  "
-# # position3_grid2_2 = "|  #{choices["8"]}  "
-# # position3_grid3_2 = "|  #{choices["9"]}  "
-
-# # puts position3_grid1_2 + position3_grid2_2 + position3_grid3_2
-
-# # position3_grid1_3 = "     "
-# # position3_grid2_3 = "|     "
-# # position3_grid3_3 = "|     "
-
-# # puts position3_grid1_3 + position3_grid2_3 + position3_grid3_3
-
-possible_choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-user_choice = gets.chomp.to_i
-
-players_choice = user_choice - 1
-
-possible_choices.slice!(players_choice)
-
-computer_choice = possible_choices.sample
-
-computer_choice_to_array_location = computer_choice - 1
-
-def format_remaining_choice(choices, user_or_computer_choice)
-  choices.slice!(user_or_computer_choice)
+def draw_board(b)
+  system "clear"
+  puts " #{b[1]} | #{b[2]} | #{b[3]} "
+  puts "---+---+---"
+  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts "---+---+---"
+  puts " #{b[7]} | #{b[8]} | #{b[9]} "
 end
 
-format_remaining_choice(possible_choices, computer_choice_to_array_location)
-# possible_choices.slice!(computer_choice_to_array_location)
+def empty_position(b)
+  b.select {|k, v| v == " "}.keys
+end
+
+def player_picks_number(b)
+  begin
+  puts "Pick a position (1-9):"
+  position = gets.chomp.to_i
+end until empty_position(b).include?(position)
+  b[position] = "X"
+end
 
 
+def computer_picks_number(b)
+  position = empty_position(b).sample
+  b[position] = "O"
+end
 
-puts "*****************************"
-puts computer_choice
+def check_winner(board)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    return "Player" if board.values_at(*line).count('X') == 3
+    return "Computer" if board.values_at(*line).count('O') == 3
+  end
+  nil
+end
 
-puts "*****************************"
+begin
+  draw_board(board)
 
-puts possible_choices 
-  
+  player_picks_number(board)
+  draw_board(board)
+  computer_picks_number(board)
+  winner = check_winner(board)
+end until winner || empty_position(board).empty?
 
-
-
+if winner
+  puts "#{winner} Won!"
+else
+  puts "Its a tie"
+end
