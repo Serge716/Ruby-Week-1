c3bcdfdbb1bb055907af514a5521dd463e114b22
Sergino Delia <serge716@gmail.com>
2015-01-05 08:16:09 -0500

    Initial Commit

---
+++ blackjack.rb
@@ -0,0 +1,26 @@
+# Logic
+# 1) Represent a deck of 52 cards
+ 
+# 2)  4 suites from low to high:
+#       clubs
+#       diamonds
+#       hearts
+#       spades
+
+# 3)  card value:
+#     1 to 10 = face value
+#     J to K = 10
+#     Ace = 1 or 11
+
+# 4)  Dealer deals a card to the player and a facedown cards to self
+# 5)  Dealer deals another card to player and a faceup card to self
+
+# 6) Player evaluate card value
+#     choose hit or stay
+#     hit: player is given another card
+#     stay: player card value is compared to dealer card
+#     if player card value is higher tha dealer card
+#       dealer deals another card to self
+
+# 7)  player wins if vard value 21 or greater than dealer value
+# 8)  player busts if card value is greater than 21
